0af34a85ed278109f0f9428c1a647da3f7006c81
Zeyad Issa (tier0) <tier0\zeyad.issa@ec2amaz-fyi4yb.tier0.prd.thf.aws>
2024-03-13 10:13:42 +0000

    add ae

---
+++ src/ae_data.R
@@ -0,0 +1,20 @@
+ae_links <- GetLinks(ae_url,'/ae-attendances-and-emergency-admissions')
+ae_files <- GetLinks(ae_links,'.csv')
+
+ae_data <- lapply(ae_files,
+                  function(x){
+                    data <- data.table::fread(x) %>%
+                      dplyr::mutate(name = x) %>%
+                      janitor::clean_names() %>%
+                      dplyr::rowwise() %>%
+                      dplyr::group_by(period,org_code) %>%
+                      dplyr::summarise(ae_attendances = sum(c_across(contains('a_e_attendances'))),
+                                       ae_breaches = sum(c_across(contains('over_4hrs'))),
+                                       ae_admissions = sum(c_across(contains('emergency_admissions'))),
+                                       admission_breaches = sum(c_across(contains('patients_who'))))
+                  }) %>%
+                      data.table::rbindlist()
+
+FINAL_ae_data <- ae_data %>%
+  dplyr::filter(period != 'TOTAL' & period != '') %>%
+  dplyr::mutate(period = lubridate::my(substr(period,8,nchar(period))))
--- src/workforce.R
+++ src/workforce.R
@@ -3,6 +3,16 @@ workforce_links <- GetLinks(workforce_url,'nhs-workforce-statistics')
 workforce_links2 <- GetLinks(paste0('https://digital.nhs.uk/',workforce_links),'.zip')
 
 #read data
+
+#Read all csvs from urls; unz for zips
+UnzipCSV <- function(files){
+  #creates temp file to read in the data
+  temp <- tempfile()
+  download.file(files,temp,extdir=tempdir())
+  #unlink the temp file, important to do
+  unlink(temp)
+  files_names}
+
 #There are embedded nuls in the data. have to clean. pls fix nhs digital
 workforce_data <- lapply(workforce_links2,
                          function(x){
